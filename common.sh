7558bc6138bf703e83b676aceada47f1ec0a339b
Ivan Marinov <imarinov@gmail.com>
2022-10-16 19:45:47 +0300

    Update common.sh

--- common.sh
+++ common.sh
@@ -9,7 +9,7 @@ sudo modprobe br_netfilter
 sudo cat << EOF | tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
-sudo cat << EOF | tee /etc/sysctl.d/k8s.conf
+cat << EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
